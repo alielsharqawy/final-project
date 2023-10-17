034f661daa10e53834a837de907b6f074c036d4b
AhmedSlman <ahmedslman151@gmail.com>
2023-10-17 15:40:03 +0300

    organized files

---
+++ lib/widgets/settings/privacy/welcom_container.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WelcomContainer extends StatelessWidget {
+  const WelcomContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 240,
+      width: 500,
+      color: Colors.lightBlue,
+    );
+  }
+}
