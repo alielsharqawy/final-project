510ed021fcd3ad9b2e8f6a441afcffee49b7fd21
AhmedSlman <ahmedslman151@gmail.com>
2023-10-17 15:32:47 +0300

    final organization

---
+++ lib/utlis/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static Color white = const Color(0xffffffff);
+  static Color lightColor = const Color(0xffF5F5F5);
+  static Color lowPriority = const Color.fromARGB(255, 0, 0, 0);
+  static Color primaryColor = const Color.fromARGB(255, 255, 72, 17);
+  static Color secondaryColor = const Color(0xff282162);
+  static Color darkColor = const Color(0xff000036);
+  static Color scaffoldColor = const Color.fromARGB(255, 0, 0, 0);
+  static Color appBarColor = const Color.fromARGB(255, 16, 16, 16);
+  static Color primaryLight = const Color.fromARGB(255, 16, 16, 19);
+}
